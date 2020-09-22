ef706fc64872ec71a467cfa57cc60533b37ea0b2
dark <dark@DESKTOP-F0STRUP>
2020-09-22 17:01:15 +0300

    no message

---
+++ Cinema/Cinema/Controllers/TestOOPController.cs
@@ -0,0 +1,28 @@
+﻿using Cinema.Models;
+using Cinema.Models.TestOOP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cinema.Controllers
+{
+    public class TestOOPController : Controller
+    {
+        public ActionResult TestInheritance()
+        {
+            var testVariable = new TestClass();
+            TestIntefaceParam(testVariable);
+            return null;
+        }
+
+
+        private string TestIntefaceParam(TestInterface implemantation)
+        {
+            var test = (TestClass)implemantation;
+            return null;
+        }
+
+    }
+}
---
+++ Cinema/Cinema/Models/TestOOP/Test2Class.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Models.TestOOP
+{
+    public class Test2Class
+    {
+        public int TestInt { get; set; }
+
+        public string TestString { get; set; }
+
+        public Test2Class()
+        {
+
+        }
+        public Test2Class(int testInt)
+        {
+            this.TestInt = testInt;
+        }
+        public Test2Class(string testString)
+        {
+            this.TestString = testString;
+        }
+        public Test2Class(int testInt, string testString)
+        {
+            this.TestInt = testInt;
+            this.TestString = testString;
+        }
+    }
+}
