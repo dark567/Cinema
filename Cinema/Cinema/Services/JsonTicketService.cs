88fa4981a9301111adeee9a28929bc89c90deef4
dark <dark@DESKTOP-F0STRUP>
2020-09-27 23:55:52 +0300

    6

---
+++ Cinema/Cinema/Services/JsonTicketService.cs
@@ -0,0 +1,76 @@
+﻿using Cinema.Models.Tickets;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Services
+{
+    public class JsonTicketService : ITicketService
+    {
+        private HttpContext Context { get; set; }
+        private const string PathToJson = "/Bd/Tickets.json";
+
+        public JsonTicketService(HttpContext context)
+        {
+            Context = context;
+        }
+
+        public Hall[] GetAllHalls()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Movie[] GetAllMovies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tariff[] GetAllTariffs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TimeSlot[] GetAllTimeSlots()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Hall GetHallById(int id)
+        {
+            var fullModel = GetDataFromFile();
+            return fullModel.Halls.FirstOrDefault(halls => halls.Id == id);
+        }
+
+        public Movie GetMovieById(int id)
+        {
+            var fullModel = GetDataFromFile();
+            return fullModel.Movies.FirstOrDefault(movie => movie.Id == id);
+        }
+
+        public Tariff GetTariffById(int id)
+        {
+            var fullModel = GetDataFromFile();
+            return fullModel.Tariffs.FirstOrDefault(tariffs => tariffs.Id == id);
+        }
+
+        public TimeSlot GetTimeSlotById(int id)
+        {
+            var fullModel = GetDataFromFile();
+            return fullModel.TimeSlots.FirstOrDefault(timeSlots => timeSlots.Id == id);
+        }
+
+        private TicketsJsonModel GetDataFromFile()
+        {
+            var jsonFilePath = Context.Server.MapPath(PathToJson);
+            if (!System.IO.File.Exists(jsonFilePath))
+                return null;
+
+            var jsonModel = System.IO.File.ReadAllText(jsonFilePath);
+            var deserializedModel = JsonConvert.DeserializeObject<TicketsJsonModel>(jsonModel);
+            return deserializedModel;
+
+        }
+    }
+}
