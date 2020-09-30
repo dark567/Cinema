1f7d1ba3eda3b45294b2a97b40785dfcbe328e77
dark <dark@DESKTOP-F0STRUP>
2020-09-30 21:32:00 +0300

    no message

---
+++ Cinema/Cinema/Models/Tickets/SubNavigationMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cinema.Models.Tickets
+{
+    public enum SubNavigationMenu
+    {
+        Movies,
+        TimeSlots,
+        Tariffs,
+        Halls
+    }
+}
