15b31a2a9d866ca804da79a9415b0095a2cf0124
dark <dark@DESKTOP-F0STRUP>
2020-10-01 17:50:34 +0300

    no message

--- Cinema/Cinema/App_Start/BundleConfig.cs
+++ Cinema/Cinema/App_Start/BundleConfig.cs
@@ -20,10 +20,13 @@ namespace Cinema
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/moment-with-locales.min.js",
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-datetimepicker.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-datetimepicker.css",
                       "~/Content/site.css"));
         }
     }
